2cb510f8466aa17e034fea3da76dc8c41d06d1bd
hryku <empf528@gmail.com>
2017-10-27 21:07:15 +0900

    crud message

---
+++ resources/views/messages/create.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+    
+    <h1>メッセージ新規作成</h1>
+
+    {!! Form::model($message, ['route' => 'messages.store']) !!}
+
+        {!! Form::label('content', 'メッセージ:') !!}
+        {!! Form::text('content') !!}
+
+        {!! Form::submit('投稿') !!}
+
+     {!! Form::close() !!}
+
+
+
+
+@endsection
